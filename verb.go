e3f92a6e5b9f62c0a76f2bf8e3bb48d022bdf8e2
jojo-saturo <niyitripplejoejoejoe@gmail.com>
2025-06-08 15:49:02 +0100

    comment the values of the verbs and changing deleting %X

--- verb.go
+++ verb.go
@@ -3,16 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	// Intergers Formatting Verbs
 
 	var Num int = 42
 
-	fmt.Printf("%b \n", Num)
-	fmt.Printf("%d \n", Num)
-	fmt.Printf("%o \n", Num)
-	fmt.Printf("%x \n", Num)
-	fmt.Printf("%X \n", Num)
-	fmt.Printf("%X \n", Num)
-	fmt.Printf("%#x \n", Num)
+	fmt.Printf("%b \n", Num)  // Base 2
+	fmt.Printf("%d \n", Num)  // Base 10
+	fmt.Printf("%o \n", Num)  // Base 8
+	fmt.Printf("%O \n", Num)  // Base 8 with leading zeros
+	fmt.Printf("%x \n", Num)  // Base 16 Lowercase
+	fmt.Printf("%X \n", Num)  // Base 16 Uppercase
+	fmt.Printf("%#x \n", Num) // Base 16 Lowercase with leading 0x
 	fmt.Printf("%4d \n", Num)
 	fmt.Printf("%-4d \n", Num)
 	fmt.Printf("%O4d \n", Num)
